55ca11044db96f018f59e938ea1f54fd658bc68f
Sagar <116898728+sagarrdec@users.noreply.github.com>
2023-02-02 22:36:57 +0530

    First commit

---
+++ Practice/Eligible.java
@@ -0,0 +1,8 @@
+public class Eligible {
+    public static void main(String[] args) {
+        System.out.println("enter your age for checking you are eligible or not: ");
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
